305ccd2ac7838b0799f351adfbf27f64fb0aaceb
ML Engineer <12114956+paulo-batista@users.noreply.github.com>
2022-04-11 15:12:54 -0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -2,4 +2,4 @@ from apicb.authenticated_client import AuthenticatedClient
 from apicb.public_client import PublicClient
 from apicb.websocket_client import WebsocketClient
 from apicb.order_book import OrderBook
-from apicb.api-coinbase_auth import coinbaseAuth
+from apicb.apicb_auth import coinbaseAuth
